3e26c5748a8b433c33d4557fd9bf7e1584c1cfb8
Bryan Byrne <brybyrne@cisco.com>
2018-04-03 12:25:35 -0400

    Initial commit

---
+++ loops.py
@@ -0,0 +1,7 @@
+student_names = ["James", "Katerina", "Jessica", "Mark", "Bort", "Frank Grimes", "Max Power"]
+
+for name in student_names:
+    if name == "Bort":
+        continue
+        print("Found him! " + name)
+    print("Currently testing " + name)
